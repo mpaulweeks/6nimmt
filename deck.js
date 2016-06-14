0fbf293c545d5ad3f0e7eeeb4c18517a2c156276
mpaulweeks <mpaulweeks@gmail.com>
2016-06-13 21:37:50 -0400

    deck logic

---
+++ deck.js
@@ -0,0 +1,54 @@
+
+function Deck(){
+    
+    function make_card(number){
+        var self = {};
+        var penalty = 1;
+        if (number == 55){
+            penalty = 7;
+        } else if (number % 11 == 0){
+            penalty = 5;
+        } else if (number % 10 == 0){
+            penalty = 3;
+        } else if (number % 5 == 0){
+            penalty = 2;
+        }
+        self.number = number;
+        self.penalty = penalty;
+        return self;
+    }
+
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length; i; i -= 1) {
+            j = Math.floor(Math.random() * i);
+            x = a[i - 1];
+            a[i - 1] = a[j];
+            a[j] = x;
+        }
+    }
+
+    function make_deck(){
+        var deck = [];
+        for (var i = 1; i <= 104; i++){
+            deck.push(make_card(i));
+        }
+        shuffle(deck);
+        return deck;
+    }
+
+    var self = {};
+    self._deck = make_deck();
+    self.draw = function(number_cards){
+        number_cards = number_cards || 1;
+        var out = [];
+        for (var i = 0; i < number_cards; i++){
+            out.push(self._deck.pop());
+        }
+        if (number_cards == 1){
+            return out[0];
+        }
+        return out;
+    };
+    return self;
+}
---
+++ game.js
@@ -0,0 +1,7 @@
+
+// requires Deck
+
+function game(){
+
+
+}
